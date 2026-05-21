e530c7743e8c494bfa20b7bf7fab89465d8c5d2f
trumansacco <156451821+trumansacco@users.noreply.github.com>
2026-05-20 21:36:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import Navbar from "@/components/Navbar"
 import { Button } from "@/components/ui/button"
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import {
   Card,
   CardContent,
